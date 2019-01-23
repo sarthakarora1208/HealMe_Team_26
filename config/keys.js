c679e65056048e002287eabbef7bc4cc11018e08
Sarthak <sarthakarora1208@gmail.com>
2019-01-23 14:48:46 +0530

    fixed medicinelist

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,6 @@
 module.exports = {
   MongoURI:
-    "mongodb://@lol-shard-00-00-qadgb.mongodb.net:27017,lol-shard-00-01-qadgb.mongodb.net:27017,lol-shard-00-02-qadgb.mongodb.net:27017/test?ssl=true&replicaSet=Lol-shard-0&authSource=admin&retryWrites=true",
+    "mongodb://sarthakarora1208:warmonger@lol-shard-00-00-qadgb.mongodb.net:27017,lol-shard-00-01-qadgb.mongodb.net:27017,lol-shard-00-02-qadgb.mongodb.net:27017/test?ssl=true&replicaSet=Lol-shard-0&authSource=admin&retryWrites=true",
   SendGridUsername: "thesarthakarora@gmail.com",
-  SendGridPassword: "",
-}
+  SendGridPassword: "wimzfsoeqxhyfkbh",
+};
